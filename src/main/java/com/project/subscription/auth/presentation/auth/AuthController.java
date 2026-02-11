b41fc592e33a7f9995fa67fe680021809e817356
nathan <nathan960307@gmail.com>
2026-02-11 21:39:58 +0900

    feat(auth): auth controller 스켈레톤 구성 (#3) [2026-02-11]

---
+++ src/main/java/com/project/subscription/auth/presentation/auth/AuthController.java
@@ -0,0 +1,32 @@
+package com.project.subscription.auth.presentation.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+//    private final AuthService authService;
+
+    // 로그인
+    @PostMapping("/login")
+    public void login() {
+
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public void logout() {
+
+    }
+
+    // 토큰 재발급
+    @PostMapping("/reissue")
+    public void reissue() {
+
+    }
+}
