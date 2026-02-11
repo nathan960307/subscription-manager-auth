3d6d300c9c88ba8f67877fffa65c4b4a4886b84d
nathan <nathan960307@gmail.com>
2026-02-11 17:54:24 +0900

    feat(auth): email로 user entity 조회하는 메서드 추가 (#2) [2026-02-11]

--- src/main/java/com/project/subscription/auth/repository/user/UserRepository.java
+++ src/main/java/com/project/subscription/auth/repository/user/UserRepository.java
@@ -3,5 +3,10 @@ package com.project.subscription.auth.repository.user;
 import com.project.subscription.auth.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
