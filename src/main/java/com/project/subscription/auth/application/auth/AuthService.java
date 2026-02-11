185861e8a44799803dc3c50392b6c975d22ab26f
nathan <nathan960307@gmail.com>
2026-02-11 22:05:00 +0900

    feat(auth): auth service 스켈레톤 구성 (#3) [2026-02-11]

---
+++ src/main/java/com/project/subscription/auth/application/auth/AuthService.java
@@ -0,0 +1,25 @@
+package com.project.subscription.auth.application.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    // 로그인
+    public void login(){
+
+    }
+
+    // 로그아웃
+    public void logout(){
+
+    }
+
+    // 토큰 재발급
+    public void reissue(){
+
+    }
+
+}
