c1f07841f582a0b93d6f6ef5a70b655618d926f2
nathan <nathan960307@gmail.com>
2026-02-11 17:09:38 +0900

    init: subscription auth server base project

---
+++ src/main/java/com/project/subscription/auth/SubscriptionAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.subscription.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionAuthServerApplication.class, args);
+    }
+
+}
