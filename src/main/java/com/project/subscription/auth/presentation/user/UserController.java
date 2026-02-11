29bd373b3b624080ee074df49676212ed8439941
nathan <nathan960307@gmail.com>
2026-02-11 19:18:43 +0900

    feat(auth): user controller 스켈레톤 구성 (#2) [2026-02-11]

---
+++ src/main/java/com/project/subscription/auth/presentation/user/UserController.java
@@ -0,0 +1,64 @@
+package com.project.subscription.auth.presentation.user;
+
+import com.project.subscription.auth.application.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    /// ===========================
+    /// 👤 일반 사용자 영역
+    /// ===========================
+
+    // 회원가입
+    @PostMapping("/signup")
+    public void signup(){
+
+    }
+
+    // 내 정보 조회
+    @GetMapping("/me")
+    public void getMyInfo(){
+
+    }
+
+    // 정보수정
+    @PatchMapping("/me")
+    public void updateMyInfo(){
+
+    }
+
+    // 회원탈퇴
+    @DeleteMapping("/me")
+    public void deleteMyInfo(){
+
+    }
+
+    /// ===========================
+    /// 👤 관리자 영역
+    /// ===========================
+
+    // 전체 사용자 조회
+    @GetMapping
+    public void getAllUsers(){
+
+    }
+
+    // 특정 사용자 조회
+    @GetMapping("/{id}")
+    public void getUser(@PathVariable Long id){
+
+    }
+
+    // 특정 사용자 삭제
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+
+    }
+
+}
