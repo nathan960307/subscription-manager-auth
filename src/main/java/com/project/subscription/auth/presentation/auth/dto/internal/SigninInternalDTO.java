4442094475762b6b228884445077df226fe5e0c9
nathan <nathan960307@gmail.com>
2026-03-15 01:46:04 +0900

    feat(auth): signin 내부용 dto 생성(#3) [2026-03-15]

---
+++ src/main/java/com/project/subscription/auth/presentation/auth/dto/internal/SigninInternalDTO.java
@@ -0,0 +1,12 @@
+package com.project.subscription.auth.presentation.auth.dto.internal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SigninInternalDTO {
+
+    private String accessToken;
+    private String refreshToken;
+}
