ba183e8b5f91fc92df25b5dda56e71ee2dcad8e1
nathan <nathan960307@gmail.com>
2026-03-15 00:19:38 +0900

    feat(auth): at 생성 로직 완성(#3) [2026-03-15]

---
+++ src/main/java/com/project/subscription/auth/infrastructure/jwt/JwtProvider.java
@@ -0,0 +1,44 @@
+package com.project.subscription.auth.infrastructure.jwt;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    // secret key 설정, 32자 이상의 충분히 긴 문자 사용
+    private final String secretKey = "my-secret-key";
+
+    // access token 만료 시간 설정
+    private final long accessTokenExpire = 1000 * 60 * 30; // 1초(1000ms) * 60 * 30 = 30분
+
+    // at 생성
+    public String createAccessToken(Long userId) {
+
+        // 시각 객체 생성
+        Date now = new Date(); // 현재 시간 객체 생성
+        Date expireDate = new Date(now.getTime() + accessTokenExpire); // 만료 시각 객체 생성
+
+        // jwt 객체 조립
+        String accessToken = Jwts.builder()
+                .setSubject(userId.toString())
+                .setIssuedAt(now) // 토큰 생성 시각
+                .setExpiration(expireDate) // 토큰 만료 시각
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+
+        return accessToken;
+    }
+
+    // rt 생성
+
+    // token 검증
+    // 1. 변조되지 않았는지
+    // 2. 만료되지 않았는지
+
+    // token에서 user id 추출
+}
