20b987606bf231685b2e2d78a58967f85abcf1b4
nathan <nathan960307@gmail.com>
2026-03-15 23:08:27 +0900

    feat(auth): security filter chain 설정(#3) [2026-03-15]

--- src/main/java/com/project/subscription/auth/config/SecurityConfig.java
+++ src/main/java/com/project/subscription/auth/config/SecurityConfig.java
@@ -1,15 +1,49 @@
 package com.project.subscription.auth.config;
 
+import com.project.subscription.auth.infrastructure.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    // 비밀번호 암호화 bean
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // security filter 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // csrf 설정 비활성화
+                .csrf(csrf -> csrf.disable())
+
+                // 세션 사용 비활성화
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // URL 접근 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/signin").permitAll()
+                        .requestMatchers("/auth/refresh").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // JWT 필터 등록
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
 }
