f950c14eed37c0841400643db234574b03fabb63
nathan <nathan960307@gmail.com>
2026-03-15 23:35:15 +0900

    feat(auth): redis 연결 설정 (#3) [2026-03-15]

--- src/main/java/com/project/subscription/auth/config/RedisConfig.java
+++ src/main/java/com/project/subscription/auth/config/RedisConfig.java
@@ -2,6 +2,8 @@ package com.project.subscription.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
