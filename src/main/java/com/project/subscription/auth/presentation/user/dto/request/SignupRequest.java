79eed2d1f3ff1e893603ae12cf70727287a2c2a3
nathan <nathan960307@gmail.com>
2026-03-16 13:54:39 +0900

    feat(user): 회원가입 request dto 추가 (#2) [2026-03-16]

---
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/request/SignupRequest.java
@@ -0,0 +1,13 @@
+package com.project.subscription.auth.presentation.user.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest{
+
+    private String email;
+    private String password;
+    private String name;
+}
