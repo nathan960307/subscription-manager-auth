d9324b2e8af3fcf7d4db0c84624b5264aac4698d
nathan <nathan960307@gmail.com>
2026-03-16 14:16:14 +0900

    feat(user): 회원가입 service api 완성 (#2) [2026-03-16]

--- src/main/java/com/project/subscription/auth/application/user/UserService.java
+++ src/main/java/com/project/subscription/auth/application/user/UserService.java
@@ -1,12 +1,36 @@
 package com.project.subscription.auth.application.user;
 
+import com.project.subscription.auth.domain.user.User;
+import com.project.subscription.auth.presentation.user.dto.request.SignupRequest;
 import com.project.subscription.auth.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // 회원가입
+    @Transactional
+    public void signup(SignupRequest request) {
+
+        // 이메일 중복 검사
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("이미 존재하는 이메일입니다.");
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        // user 생성(도메인 메서드)
+        User user = User.createUser(request.getEmail(), encodedPassword);
+
+        // 저장
+        userRepository.save(user);
+    }
 }
