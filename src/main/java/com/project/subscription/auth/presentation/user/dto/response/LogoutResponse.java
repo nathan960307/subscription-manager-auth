aea41eac12c31831439b48ea58bf14e0aba100a0
nathan <nathan960307@gmail.com>
2026-03-16 23:41:23 +0900

    feat(autn): 로그아웃 response 생성 #3[2026-03-16]

---
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/response/LogoutResponse.java
@@ -0,0 +1,23 @@
+package com.project.subscription.auth.presentation.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@AllArgsConstructor
+public class LogoutResponse {
+
+    private final String message;
+    private final OffsetDateTime timestamp;
+
+
+    public static LogoutResponse success() {
+
+        return new LogoutResponse(
+                "로그아웃 성공",
+                OffsetDateTime.now()
+        );
+    }
+}
