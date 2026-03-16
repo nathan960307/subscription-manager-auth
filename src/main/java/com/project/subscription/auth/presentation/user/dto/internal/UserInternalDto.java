8fec57ca8996c365ed51bc83ff249139531175a6
nathan <nathan960307@gmail.com>
2026-03-16 15:03:38 +0900

    feat(user): 내정보 조회용 내부용 dto 및 정적 팩토리 메서드 완성[2026-03-16]

---
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/internal/UserInternalDto.java
@@ -0,0 +1,25 @@
+package com.project.subscription.auth.presentation.user.dto.internal;
+
+import com.project.subscription.auth.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserInternalDto {
+
+    // 필드
+    String email;
+    String role;
+
+    // 정적 팩토리 메서드
+    public static UserInternalDto from(User user) {
+        return  UserInternalDto.builder()
+                    .email(user.getEmail())
+                    .role(user.getRole())
+                    .build();
+    }
+
+}
