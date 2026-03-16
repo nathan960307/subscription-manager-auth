b06d4bfa0bd88ad12df9225eba7c41b06f81100d
nathan <nathan960307@gmail.com>
2026-03-16 23:10:18 +0900

    feat(user): 로그인 entity 필드 추가 #2[2026-03-16]

--- src/main/java/com/project/subscription/auth/domain/user/User.java
+++ src/main/java/com/project/subscription/auth/domain/user/User.java
@@ -27,19 +27,37 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password; // 사용자 비밀번호
 
+    @Column(name = "name")
+    private String name; // 사용자 이름
+
+    @Column(name = "phone_number")
+    private String phoneNumber; // 사용자 전화번호
+
+    @Column(name = "provider")
+    private String provider; // 로그인 제공자
+
+    @Column(name = "provider_id")
+    private String providerId; // OAuth 고유 아이디
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt; // 최근 로그인 날짜
+
     @Column(name = "role", nullable = false)
     private String role; // 역할
 
-    @Column(name ="deleted", nullable = false)
+    @Column(name = "is_deleted", nullable = false)
     private boolean deleted = false; // 삭제 여부
+
+    @Column(name = "is_blacklisted", nullable = false)
+    private boolean blacklisted = false; // 블랙리스트 여부
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt; // 생성 일자
 
     @UpdateTimestamp
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt; // 수정 일자
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt; // 삭제 일자
