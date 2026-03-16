8e138cbc92f5d66d6c116c6041b0e8dafa40ef1f
nathan <nathan960307@gmail.com>
2026-03-16 15:29:44 +0900

    feat(user): 내 정보 조회 response 완성[2026-03-16]

---
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/response/MyInfoResponse.java
@@ -0,0 +1,26 @@
+package com.project.subscription.auth.presentation.user.dto.response;
+
+import com.project.subscription.auth.presentation.user.dto.internal.UserInternalDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@AllArgsConstructor
+public class MyInfoResponse {
+
+    private final String message;
+    private final OffsetDateTime timestamp;
+    private final UserInternalDto data;
+
+
+    public static MyInfoResponse success(UserInternalDto userInternalDto) {
+
+        return new MyInfoResponse(
+                "내 정보 조회 성공",
+                OffsetDateTime.now(),
+                userInternalDto
+        );
+    }
+}
