829804e6dc9b59f39f7ee8651a5451115634599d
nathan <nathan960307@gmail.com>
2026-03-16 17:17:49 +0900

    feat(user): 단일 사용자 조회 response 추가, 및 generic 타입으로 변경 #2[2026-03-16]

--- src/main/java/com/project/subscription/auth/presentation/user/dto/response/AdminResponse.java
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/response/AdminResponse.java
@@ -1,7 +1,6 @@
 package com.project.subscription.auth.presentation.user.dto.response;
 
 import com.project.subscription.auth.presentation.user.dto.internal.AdminUserInternalDto;
-import com.project.subscription.auth.presentation.user.dto.internal.UserInternalDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,19 +9,28 @@ import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class AdminResponse {
+public class AdminResponse<T> {
 
     private final String message;
     private final OffsetDateTime timestamp;
-    private final List<AdminUserInternalDto> data;
+    private final T data;
 
 
-    public static AdminResponse getAllUsers(List<AdminUserInternalDto> users) {
+    public static AdminResponse<List<AdminUserInternalDto>> getAllUsers(List<AdminUserInternalDto> users) {
 
-        return new AdminResponse(
+        return new AdminResponse<>(
                 "모든 사용자 조회 성공",
                 OffsetDateTime.now(),
                 users
+        );
+    }
+
+    public static AdminResponse<AdminUserInternalDto> getUser(AdminUserInternalDto user) {
+
+        return new AdminResponse<>(
+                "단일 사용자 조회 성공",
+                OffsetDateTime.now(),
+                user
         );
     }
 
