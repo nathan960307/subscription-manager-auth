fb50daf89f02d1f2845b9d945b90e4fc9228d4f6
nathan <nathan960307@gmail.com>
2026-03-17 19:07:02 +0900

    feat(config): dto 검증용 어노테이션 추가 #3[2026-03-17]

--- src/main/java/com/project/subscription/auth/presentation/auth/dto/request/RefreshRequest.java
+++ src/main/java/com/project/subscription/auth/presentation/auth/dto/request/RefreshRequest.java
@@ -1,9 +1,11 @@
 package com.project.subscription.auth.presentation.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class RefreshRequest {
 
+    @NotBlank
     private String refreshToken;
 }
