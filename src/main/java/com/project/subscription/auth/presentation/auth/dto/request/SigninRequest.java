fe11b3fc6c971f7fd4136c16222359a584f204e5
nathan <nathan960307@gmail.com>
2026-03-17 19:06:01 +0900

    feat(config): dto 검증용 어노테이션 추가 #3[2026-03-17]

--- src/main/java/com/project/subscription/auth/presentation/auth/dto/request/SigninRequest.java
+++ src/main/java/com/project/subscription/auth/presentation/auth/dto/request/SigninRequest.java
@@ -1,5 +1,7 @@
 package com.project.subscription.auth.presentation.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
