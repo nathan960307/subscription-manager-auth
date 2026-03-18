a829f299b5351a4c76c905f21bb115cafd8e8a26
nathan <nathan960307@gmail.com>
2026-03-18 12:00:07 +0900

    feat(test) : mock 객체 생성 #4 [2026-03-18]

--- src/test/java/com/project/subscription/auth/application/auth/AuthServiceTest.java
+++ src/test/java/com/project/subscription/auth/application/auth/AuthServiceTest.java
@@ -1,8 +1,29 @@
 package com.project.subscription.auth.application.auth;
 
+import com.project.subscription.auth.infrastructure.jwt.JwtProvider;
+import com.project.subscription.auth.infrastructure.redis.RedisService;
+import com.project.subscription.auth.repository.user.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtProvider jwtProvider;
+
+    @Mock
+    private RedisService redisService;
+
+    @InjectMocks
+    private AuthService authService;
 }
