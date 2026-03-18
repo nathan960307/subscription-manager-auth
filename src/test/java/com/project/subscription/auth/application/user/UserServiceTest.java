10c679e09b51799bc5d26c590759b74b00c681eb
nathan <nathan960307@gmail.com>
2026-03-18 13:32:12 +0900

    feat(test) : 회원가입 성공 테스트 메서드 완성 #5 [2026-03-18]

--- src/test/java/com/project/subscription/auth/application/user/UserServiceTest.java
+++ src/test/java/com/project/subscription/auth/application/user/UserServiceTest.java
@@ -1,11 +1,20 @@
 package com.project.subscription.auth.application.user;
 
+import com.project.subscription.auth.domain.user.User;
+import com.project.subscription.auth.presentation.user.dto.request.SignupRequest;
 import com.project.subscription.auth.repository.user.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -19,5 +28,38 @@ class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
+
+    // 테스트 메서드
+
+    // 회원가입 성공
+    @Test
+    void signup_success() {
+
+        // given
+        SignupRequest request = new SignupRequest(
+                "test@test.com",
+                "1234",
+                "홍길동",
+                "01012345678"
+        );
+
+        when(userRepository.findByEmail(any()))
+                .thenReturn(Optional.empty());
+
+        when(passwordEncoder.encode(any()))
+                .thenReturn("encoded");
+
+        // when
+        userService.signup(request);
+
+        // then
+        verify(userRepository).save(any(User.class));
+    }
+
+    // 회원가입 실패
+
+    // 내 정보 삭제(탈퇴)
+
+
 
 }
