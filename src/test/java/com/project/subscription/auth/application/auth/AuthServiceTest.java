be25655367642dfb3b12b3d305dd3231a9dcaa54
nathan <nathan960307@gmail.com>
2026-03-18 11:58:27 +0900

    feat(test) : mockito 어노테이션 추가 #4 [2026-03-18]

--- src/test/java/com/project/subscription/auth/application/auth/AuthServiceTest.java
+++ src/test/java/com/project/subscription/auth/application/auth/AuthServiceTest.java
@@ -1,4 +1,8 @@
 package com.project.subscription.auth.application.auth;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 }
