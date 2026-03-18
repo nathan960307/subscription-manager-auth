ab94a5acb6663ead3ec1d10e469b5a135ac0f992
nathan <nathan960307@gmail.com>
2026-03-18 13:32:28 +0900

    feat(test) : 모든 필드를 매개변수로 받는 생성자 어노테이션 추가 #5 [2026-03-18]

--- src/main/java/com/project/subscription/auth/presentation/user/dto/request/SignupRequest.java
+++ src/main/java/com/project/subscription/auth/presentation/user/dto/request/SignupRequest.java
@@ -1,11 +1,13 @@
 package com.project.subscription.auth.presentation.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignupRequest{
 
     @NotBlank
