3461d4259e2f241a38f8dbfca24bbc46bf90fd45
nathan <nathan960307@gmail.com>
2026-03-18 20:16:07 +0900

    feat(response) : apiresponse 클래스 필드 및 스태틱 메서드 추가 #7 [2026-03-18]

--- src/main/java/com/project/subscription/auth/global/response/ApiResponse.java
+++ src/main/java/com/project/subscription/auth/global/response/ApiResponse.java
@@ -1,4 +1,27 @@
 package com.project.subscription.auth.global.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final int code;
+    private final String message;
+    private final LocalDateTime timestamp;
+    private final T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, 2000, "성공",LocalDateTime.now(), data);
+    }
+
+    public static ApiResponse<?> fail(int code, String message) {
+        return new ApiResponse<>(false, code, message,LocalDateTime.now(), null);
+    }
+
 }
