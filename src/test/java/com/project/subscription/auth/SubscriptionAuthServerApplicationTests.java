c3096327b437da9cb3e8602ba2ea6026215a5771
nathan <nathan960307@gmail.com>
2026-03-18 23:19:06 +0900

    feat(CI) : 테스트 더미 클래스에 테스트 설정 파일 쓰게 설정 #8 [2026-03-18]

--- src/test/java/com/project/subscription/auth/SubscriptionAuthServerApplicationTests.java
+++ src/test/java/com/project/subscription/auth/SubscriptionAuthServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.project.subscription.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SubscriptionAuthServerApplicationTests {
 
     @Test
